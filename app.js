272fd259ee3b2bf06cb0718f7f7b9f54b056c7c1
Raja <sauravrajjha1999@gmail.com>
2023-09-15 22:11:29 +0530

    Updated path

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const Post = require("./db/post");
 require("./db/connection");
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(path.join(__dirname,"/public")))
 app.use(express.json({limit: '50mb'}));
 app.use(bodyparser.urlencoded({extended:true, limit: '50mb'}));
 app.use(cookieParser());
